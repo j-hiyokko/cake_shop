60a49ec9d4816ad32deb9661de6a5b12c2288fcc
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-10-17 06:26:34 +0000

    [Add]item_image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  has_one_attached :item_image
+
+  def get_image
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/NOIMAGE.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
 end
