30068bc095f81c8c56b3fe2b0cd53285f5162b17
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-10-17 06:05:57 +0000

    Add viewfile

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,36 @@
 class Admin::ItemsController < ApplicationController
+
+  def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to 'admin/items'
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_name,:introduction,:price,)
+  end
+  
 end
