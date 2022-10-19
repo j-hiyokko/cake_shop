13d3183a6dfbaa3d6f6fed2de5532ab52b7e4862
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-10-19 10:37:26 +0000

    [Fix]CartItemmodel_column/user_id => customer_id

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,31 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.save
+    redirect_to cart_items_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    @cart_items = CartItem.find(current_customer)
+    @cart_items.destroy.all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:user_id,:item_id,:quantity)
+  end
+
 end
