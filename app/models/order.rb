c43269159a6d9859bf8b05d30066cd1ecd711d03
YukaHayashi <112526288+dmm-h-y@users.noreply.github.com>
2022-10-19 21:25:14 +0000

    add models/order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,15 @@ class Order < ApplicationRecord
 
   has_many :order_details, dependent: :destroy
   belongs_to :customer
+  
+  validates :postcode, presence: true
+  validates :address, presence: true 
+  validates :address_name, presence: true 
+  validates :payment, presence: true 
+  
+  
+  def add_tax
+    (item.price * 1.08).round
+  end
+  
 end
