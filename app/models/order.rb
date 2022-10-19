8835a0cf4f6cf2cc3cfcb1ea17f2ec74704c320e
YukaHayashi <112526288+dmm-h-y@users.noreply.github.com>
2022-10-19 21:31:00 +0000

    add models/order

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,6 +8,9 @@ class Order < ApplicationRecord
   validates :address_name, presence: true 
   validates :payment, presence: true 
   
+  enum payment: { credit_card: 0, transfer: 1}
+
+  enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
   
   def add_tax
     (item.price * 1.08).round
