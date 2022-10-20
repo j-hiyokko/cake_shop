fd44afeb043986484974008caebd3f1749e800ac
kanou <112619837+taiseikanou@users.noreply.github.com>
2022-10-20 05:42:48 +0000

    [fix]order/model

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,16 +4,18 @@ class Order < ApplicationRecord
   belongs_to :customer
 
   validates :postcode, presence: true
-  validates :address, presence: true 
-  validates :address_name, presence: true 
-  validates :payment, presence: true 
-  
+  validates :address, presence: true
+  validates :address_name, presence: true
+  validates :payment, presence: true
+
   enum payment: { credit_card: 0, transfer: 1}
 
   enum order_status: { 入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
-  
+
+  enum payment: { クレジットカード: 0, 銀行振込: 1 }
+
   def add_tax
     (item.price * 1.08).round
   end
-  
+
 end
