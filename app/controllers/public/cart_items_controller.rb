693dea30cde1f8b6b6ed0da55532f51421be93d4
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-10-20 01:52:19 +0000

    [Fix]cart_item controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
     @cart_items = current_customer.cart_items.all
@@ -7,31 +8,35 @@ class Public::CartItemsController < ApplicationController
 
   def create
     if cart_item = CartItem.find_by(item_id:cart_item_params[:item_id])
-      # cart_item.quantity += (cart_item_params[:item_id][:quantity]).to_i
       cart_item.quantity += cart_item_params[:quantity].to_i
+      # <cart_item.quantity = cart_item.quantity + cart_item_params[:quantity].to_i>
       cart_item.save
     else
       cart_item = CartItem.new(cart_item_params)
       cart_item.customer_id = current_customer.id
       cart_item.save
     end
+    flash[:notice] = '商品が追加されました'
     redirect_to cart_items_path
   end
 
   def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
+    flash[:notice] = '個数が変更されました'
     redirect_to cart_items_path
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
+    flash[:notice] = '商品が削除されました'
     redirect_to cart_items_path
   end
 
   def destroy_all
     current_customer.cart_items.destroy_all
+    flash[:notice] = 'カートを空にしました'
     redirect_to cart_items_path
   end
 
