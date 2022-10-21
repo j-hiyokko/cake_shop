5c87a05c42a3572f442ce4d8bfb7aadf077557e0
tsuruta <naritakundono@gmail.com>
2022-10-21 07:52:05 +0000

    [Fix]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,27 +2,29 @@ class Public::AddressesController < ApplicationController
 
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash[:notice] = '情報が追加されました'
+      redirect_to action: :index
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
     @address = Address.find(params[:id])
   end
 
-  def create
-    @address = Address.new(address_params)
-    if @address.save
-      redirect_to action: :index
-    else
-      @address = Address.new
-      @addresses = Address.all
-      render :index
-    end
-  end
-
   def update
     @address = Address.find(params[:id])
     if @address.update(address_params)
+      flash[:notice] = '情報が変更されました'
       redirect_to action: :index
     else
       @address = Address.find(params[:id])
@@ -33,13 +35,15 @@ class Public::AddressesController < ApplicationController
   def destroy
     @address = Address.find(params[:id])
     @address.destroy
+    @address = current_customer.addresses
+    flash[:notice] = '情報が削除されました'
     redirect_to action: :index
   end
 
   private
 
   def address_params
-    params.require(:address).permit(:postcode,:address,:address_name).merge(customer_id: current_customer.id)
+    params.require(:address).permit(:postcode,:address,:address_name)
   end
 
 end
