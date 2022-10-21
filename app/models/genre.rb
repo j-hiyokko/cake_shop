972882b5321b432448ead3e07daffc0ad5efc4ee
tsuruta <naritakundono@gmail.com>
2022-10-21 10:40:05 +0000

    [Add]before_action

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
   # before_action :authenticate_user!
   has_many :items, dependent: :destroy
 
-  validates :genre_name, presence: true
+  validates :genre_name, presence: true, uniqueness: true
 end
