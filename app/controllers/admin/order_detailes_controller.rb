202f32dfbbdcf4a2ad79663b784528bd37431338
EC2 Default User <ec2-user@ip-172-31-12-173.ap-northeast-1.compute.internal>
2022-10-22 11:13:32 +0000

    address

--- app/controllers/admin/order_detailes_controller.rb
+++ app/controllers/admin/order_detailes_controller.rb
@@ -1,2 +1,25 @@
 class Admin::OrderDetailesController < ApplicationController
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+      if @order.order_status == "入金待ち"
+        @order_details.update_all(product_status: "着手不可")
+      elsif @order.orderstatus == "入金確認"
+        @order_details.update_all(product_status: "製作待ち")
+      end
+      redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
+  end
+
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:product_status)
+  end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,10 +15,10 @@ class Public::OrdersController < ApplicationController
       @order.postcode = current_customer.postcode
       @order.address_name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:address_option] == "1"
-      ship = Address.find(params[:order][:customer_id])
-      @order.address = ship.postcode
-      @order.postcode = ship.address
-      @order.address_name = ship.address_name
+      @order.address = Address.find(params[:order][:address]).address
+      @order.shipping_postcode = Destination.find(params[:order][:destination]).shipping_postcode
+      @order.postcode = Address.find(params[:order][:address]).postcode
+      @order.address_name = Address.find(params[:order][:address]).address_name
     elsif params[:order][:address_option] == "2"
       @order.address = params[:order][:address]
       @order.postcode = params[:order][:postcode]
@@ -32,6 +32,16 @@ class Public::OrdersController < ApplicationController
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new #初期化宣言
+      @order_detail.item_id = cart_item.item_id #商品idを注文商品idに代入
+      @order_detail.order_quantity = cart_item.quantity #商品の個数を注文商品の個数に代入
+      @order_detail.total_amount = cart_item.quantity.to_i*cart_item.item.price.to_i
+      @order_detail.order_id =  @order.id #注文商品に注文idを紐付け
+      @order_detail.save #注文商品を保存
+    end
+    current_customer.cart_items.destroy_all
     redirect_to complete_order_path
   end
 
@@ -48,4 +58,6 @@ class Public::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:payment,:postcode,:address,:address_name,:customer_id,:billing_amount,:order_status,:postage,:image)
   end
+
+
 end
