11b44b532ca6d13419eed03d199d43d79b43ec40
YukaHayashi <112526288+dmm-h-y@users.noreply.github.com>
2022-10-23 09:44:05 +0000

    add

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -41,6 +41,7 @@ class Public::OrdersController < ApplicationController
     @orders = Order.where(customer_id: current_customer.id)
   end
   def show
+    @order = Order.find(params[:id])
   end
 
   private
