2ee05f5842de99dad6a8026ed854f5ca55e1f75c
EC2 Default User <ec2-user@ip-172-31-12-173.ap-northeast-1.compute.internal>
2022-10-24 01:05:29 +0000

    レイアウトの変更、ステータスの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,12 +7,9 @@ class Order < ApplicationRecord
   validates :address, presence: true
   validates :address_name, presence: true
   validates :payment, presence: true
-
-
-
+  
+  
   enum payment:{ credit_card:0 ,transfer: 1}
-
-
   enum order_status:{ waiting_for_payment:0,payment_confirmation:1,under_construction:2,shipping_preparation:3,sent:4}
 
 
